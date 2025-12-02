f246a48343be65132ba8528804713bbf9d3fb05a
StaffanBaloo <staffan.johansson@gmail.com>
2025-12-02 19:16:03 +0100

    bygg struktur för produkt, sortiment, och varukorg

---
+++ app.js
@@ -0,0 +1,129 @@
+class Product {
+    //@ts-check
+    /**
+     * Skapar ett objekt med ett id-nummer, namn, kategori, och pris.
+     * @param {Number} id
+     * @param {String} namn
+     * @param {String} kategori
+     * @param {Number} pris
+     */
+    constructor(id, namn, kategori, pris) {
+        this.id = id;
+        this.namn = namn;
+        this.kategori = kategori;
+        this.pris = pris;
+    }
+
+    setNamn = (namn) => (this.namn = namn);
+
+    setPris = (pris) => (this.pris = pris);
+
+    setKategori = (kategori) => (this.kategori = kategori);
+
+    getNamn = () => this.namn;
+
+    getPris = () => this.pris;
+
+    getKategori = () => this.kategori;
+
+    getId = () => this.id;
+}
+
+class Sortiment {
+    //Skapar ett sortiment som en tom array.
+    constructor() {
+        this.sort = [];
+    }
+
+    populera() {
+        //Här ska vi ha en rutin för att läsa in en JSON-fil med sortimentet. Alternativt kanske det kan vara en del av konstruktorn?
+    }
+
+    // Lägger till en produkt i sortimentet med id, namn, kategori, och pris.
+    addProd = (id, namn, kategori, pris) =>
+        this.sort.push(new Product(id, namn, kategori, pris));
+
+    // Hämtar produkten med ett visst id. Här borde det finnas någon sorts felkontroll också ifall id:t inte finns.
+    getProd = (id) => {
+        for (const element of this.sort) {
+            if (element.getId() === id) {
+                return element;
+            }
+        }
+    };
+
+    // Nedanstående funktioner hämtar priset, namnet, samt kategorin på produkten med ett visst id.
+    getPris = (id) => this.getProd(id).getPris;
+
+    getNamn = (id) => this.getProd(id).getNamn;
+
+    getKategori = (id) => this.getProd(id).getKategori;
+}
+
+class Varukorg {
+    //Skapar en tom varukorg som en tom array. Arrayen ska sedan innehålla arrayer på formen [id, antal]. Varukorgen är även länkad till ett visst sortiment.
+    constructor(sort) {
+        this.korg = [];
+        this.sort = sort;
+    }
+
+    indexVara = (id) => {
+        // returnerar indexet för varan med ett visst id i varukorgen, eller undefined om den inte finns.
+        let i = 0;
+        while (i < this.korg.length) {
+            if (this.korg[(i, 0)] === id) return i;
+            i++;
+        }
+        return undefined;
+    };
+
+    getVara = (id) => {
+        //returnerar arrayen som har varan med ett visst id.
+        const i = this.indexVara(id);
+        if (i === undefined) {
+            return undefined;
+        } else return this.korg[i];
+    };
+
+    laggIKorg = (id, antal) => {
+        // Hämta raden i varukorgen med ett visst id. Om det inte finns, lägg in en ny rad med [id, antal]. Annars, öka antalet på den raden med antal.
+        const i = this.getVara(id);
+        if (i === undefined) {
+            this.korg.push([id, antal]);
+        } else {
+            i[1] += antal;
+        }
+    };
+
+    taUrKorg = (id, antal) => {
+        // Hämta raden i varukorgen med ett visst id. Om det inte finns, avsluta. Annars, minska antalet på den raden med antal fast som lägst till 0
+        const i = this.getVara(id);
+        if (i === undefined) {
+            return;
+        } else {
+            i[1] = Math.max(0, i[1] - antal);
+        }
+    };
+
+    delSumma = (id) => {
+        // Hämta raden i varukorgen med ett visst id. Om det inte finns, returnera 0. Annars, returnera produkten av priset för varan och antalet.
+        const i = this.getVara(id);
+        if (i === undefined) {
+            return 0;
+        } else {
+            return this.sort.getPris(id) * i[1];
+        }
+    };
+
+    totalSumma = () => {
+        // Gå genom hela varukorgen och för varje rad, räkna varupris * antal och lägg till i summan
+        let summa = 0;
+        for (let i of this.korg) {
+            summa += this.sort.getPris(i[0]) * i[1];
+        }
+        return summa;
+    };
+}
+
+const aSort = new Sortiment();
+const aKorg = new Varukorg(aSort);
